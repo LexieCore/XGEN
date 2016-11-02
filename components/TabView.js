6ba4a1726165523dc036f05cd5b6da3a9ecacff4
LexieCore <lexie@protonmail.ch>
2016-11-02 18:25:40 -0500

    sorting Alphabeticaly

--- components/TabView.js
+++ components/TabView.js
@@ -3,7 +3,7 @@ import {PropTypes} from "react";
 import {StyleSheet, Text, View} from "react-native";
 import Button from 'react-native-button';
 import { Actions } from 'react-native-router-flux';
-
+import ListViewDemo from './ListViewDemo';
 const contextTypes = {
   drawer: React.PropTypes.object,
 };
@@ -29,17 +29,7 @@ const TabView = (props, context) => {
   const drawer = context.drawer;
   return (
     <View style={[styles.container, props.sceneStyle ]}>
-      <Text>Tab {props.title}</Text>
-      {props.name === 'tab1_1' &&
-        <Button onPress={Actions.tab1_2}>next screen for tab1_1</Button>
-      }
-      {props.name === 'tab2_1' &&
-        <Button onPress={Actions.tab2_2}>next screen for tab2_1</Button>
-      }
-      <Button onPress={Actions.pop}>Back</Button>
-      <Button onPress={() => { drawer.close(); Actions.tab1(); }}>Switch to tab1</Button>
-      <Button onPress={() => { drawer.close(); Actions.tab2(); }}>Switch to tab2</Button>
-      <Button onPress={() => { drawer.close(); Actions.echo(); }}>push new scene</Button>
+      <ListViewDemo/>
     </View>
   );
 };
