25ea92ae714522df413e08d9a2de7dd917f66247
LexieCore <lexie@protonmail.ch>
2016-11-02 15:47:06 -0500

    Contactos + Grupos

---
+++ index.android.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {  AppRegistry } from 'react-native';
+import Xgen from './Xgen'
+
+AppRegistry.registerComponent('xgen', () => Xgen);
