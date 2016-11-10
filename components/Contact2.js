3a424c1266ce812f233caa806478d7bd438fc1fb
LexieCore <lexie@protonmail.ch>
2016-11-10 03:19:17 -0600

    binding Carl

--- components/Contact2.js
+++ components/Contact2.js
@@ -24,9 +24,21 @@ const NOMBRE = "UID123_object"
 
 class Contact2 extends React.Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      phone:"",
+      email:"",
+      errors:[],
+    }
+
+  }
+
   async storeContact(contact){
     try {
-      await AsyncStorage.setItem(NOMBRE,contact);
+      await AsyncStorage.setItem(NOMBRE, JSON.stringify(contact));
       this.getContact();
     } catch (error) {
       alert("somenthing went wrong");
@@ -44,8 +56,7 @@ class Contact2 extends React.Component {
 
   async anadirPressed(){
     try {
-      this.storeContact("alejandra");
-
+      this.storeContact(this.state.name);
     } catch (error) {
       alert("somenthing went wrong");
     }
@@ -56,8 +67,6 @@ class Contact2 extends React.Component {
     const state = this.props.navigationState;
     const children = state.children;
 
-
-
     return (
       <View >
             <Text style={{marginTop:50,marginLeft:50,width: 200,height: 50,color:'black'}}>Nombre</Text>
@@ -65,18 +74,21 @@ class Contact2 extends React.Component {
                 maxLength={15}
                 multiline = {true}
                 style={{marginLeft:50,width: 200,height: 50,color:'black',marginBottom:10,marginTop:0}}
+                onChangeText={ (val) => this.setState({name:val})}
                 />
             <Text style={{marginLeft:50,width: 200,height: 50,color:'black'}}>Telefono</Text>
             <TextInput
                 maxLength={15}
                 multiline = {true}
                 style={{marginLeft:50,width: 200,height: 50,color:'black',marginBottom:10}}
+                onChangeText={ (val) => this.setState({phone:val})}
                 />
             <Text style={{marginLeft:50,width: 200,height: 50,color:'black'}}>Correo Electronico</Text>
             <TextInput
                 maxLength={15}
                 multiline = {true}
                 style={{marginLeft:50,width: 200,height: 50,color:'black',marginBottom:50}}
+                nChangeText={ (val) => this.setState({email:val})}
                 />
                 <FormButton
                 isDisabled={false}
